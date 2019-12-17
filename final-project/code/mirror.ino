98f314e7723a904fd933fcbd2dec6e91a79648b8
gabibranche <54946601+gabibranche@users.noreply.github.com>
2019-12-17 11:04:48 -0500

    Update mirror.ino

--- final-project/code/mirror.ino
+++ final-project/code/mirror.ino
@@ -1,66 +1,55 @@
-/*
-  Button
+// code to make the self cleaning mirror work
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
 
-  The circuit:
-  - LED attached from pin 13 to ground
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
+// the duster works by using a servo motor and magnest which moves in one direction then the other
+// it is controlled using a remote control triggering an IR sensor
 
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
-*/
+//include library for operating servo
 #include <Servo.h>
-
 Servo myservo;
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = A0;     // the number of the pushbutton pin
-const int ledPin =  3;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 int pos = 0;
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 
+  // this pin will determin whether the remote has been pressed using an IR Sensor
   myservo.attach(11);
 }
 
 void loop() {
-  // read the state of the pushbutton value:
+  
+  // read the state of the IR Sensor value:
   buttonState = analogRead(buttonPin);
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  //print the value read by the IR Sensor
+  Serial.println(buttonState);
+  
+   // check if the pushbutton is pressed. If it is, the buttonState is more than 500:
   if (buttonState > 550) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    for (pos = 50; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
+    
+    //creating a pendulum movement with servo 
+    
+    //moves servo in a arched position allowing for cloth to movie in that arch on 'mirror'
+     for (pos = 50; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);                       // waits 15ms for the servo to reach the position
     }
+    
+     //moves servo in a arched position in the opposite direction allowing for cloth to movie in that arch on 'mirror'
     for (pos = 100; pos >= 50; pos -= 1) { // goes from 180 degrees to 0 degrees
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);                       // waits 15ms for the servo to reach the position
     }
   } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+    // if remote off botton pressed, IR sensor will read less than 500 and then servo will be turned off
   }
 }
